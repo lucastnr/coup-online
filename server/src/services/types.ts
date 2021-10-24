adea5346b4ae13abd59f85f46d262abd0d02cf85
Lucas <tnrlucas@gmail.com>
2021-10-24 20:53:42 -0300

    Remove interface Card

--- server/src/services/types.ts
+++ server/src/services/types.ts
@@ -1,9 +1,5 @@
+import Card from "../classes/Card";
 
 export type CardName = "assassino" | "capitao" | "condensa" | "duque" | "embaixador";
 export type Port = number | string;
 export type Cards = Card[];
-
-export interface Card {
-  name: CardName
-  dead: boolean;
-}
