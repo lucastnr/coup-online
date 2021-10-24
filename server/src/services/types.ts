24b8ecf509bb86a64136a9c0e4a01553c5fc42e3
Lucas <tnrlucas@gmail.com>
2021-10-24 19:56:52 -0300

    Cria estruturas de tipos

--- server/src/services/types.ts
+++ server/src/services/types.ts
@@ -1,1 +1,9 @@
+
+export type CardName = "assassino" | "capitao" | "condensa" | "duque" | "embaixador";
 export type Port = number | string;
+export type Hand = [ Card, Card ];
+
+export interface Card {
+  name: CardName
+  dead: boolean;
+}
