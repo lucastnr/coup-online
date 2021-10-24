d9691c381d28d7705365edc4e113f0bed27bbc0d
Lucas <tnrlucas@gmail.com>
2021-10-24 20:27:13 -0300

    Cria função de escolher carta

---
+++ server/src/services/utils.ts
@@ -0,0 +1,14 @@
+import { Hand } from "./types";
+
+export function randomIntFromInterval(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const pickCard = (cards: Hand) => {
+  const cardsLen = cards.length;
+  const selectedIndex = randomIntFromInterval(0, cardsLen);
+  const selectCard = cards[selectedIndex];
+  cards.splice(selectedIndex, 1);
+
+  return [selectCard, cards];
+}
