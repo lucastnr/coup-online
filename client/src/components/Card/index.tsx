563da864aeb7ed5c4318355cc75d045384d2f23b
Lucas <tnrlucas@gmail.com>
2021-10-24 15:12:22 -0300

    Cria componente Card

---
+++ client/src/components/Card/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import assassino from "../../images/cards/assassino.jpg";
+import capitao from "../../images/cards/capitao.jpg";
+import condensa from "../../images/cards/condensa.jpg";
+import duque from "../../images/cards/duque.jpg";
+import embaixador from "../../images/cards/embaixador.jpg";
+import { CardType } from '../../services/types';
+
+type HTMLImage = React.ImgHTMLAttributes<HTMLImageElement>;
+
+interface ICardProps extends HTMLImage {
+  card: CardType;
+}
+
+const cardFile: Record<CardType, string> = {
+  assassino,
+  capitao,
+  condensa,
+  duque,
+  embaixador
+}
+
+
+const Card = (props: ICardProps) => {
+  const { card } = props;
+  /** Source da imagem da carta */
+  const cardSource = cardFile[card];
+
+  return (
+    <img {...props} src={cardSource}></img>
+  )
+}
+
+export default Card;
