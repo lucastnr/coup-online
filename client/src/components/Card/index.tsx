a6abf41f26b3f5d2f36e436ce5f37f27c5befde9
Lucas <tnrlucas@gmail.com>
2021-10-26 00:23:05 -0300

    Cria estilo de carta

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
 import assassino from "../../images/cards/assassino.jpg";
 import capitao from "../../images/cards/capitao.jpg";
 import condensa from "../../images/cards/condensa.jpg";
 import duque from "../../images/cards/duque.jpg";
 import embaixador from "../../images/cards/embaixador.jpg";
+import { cardRatio } from '../../services/constants';
 import { CardName } from '../../services/types';
 
 /** Tipo que qualquer img pode receber */
@@ -11,7 +13,9 @@ type HTMLImage = Omit<React.ImgHTMLAttributes<HTMLImageElement>, "src">;
 
 interface ICardProps extends HTMLImage {
   card: CardName;
+  width?: string;
 }
+const cardDesktopDefaultWidth = "140px";
 
 const cardFile: Record<CardName, string> = {
   assassino,
@@ -21,14 +25,32 @@ const cardFile: Record<CardName, string> = {
   embaixador
 }
 
+const getCardSource = (props: ICardProps) =>
+  cardFile[props.card];
+
+const getCardDimensionsStyle = (props: ICardProps) => {
+  const { width } = props;
+
+  const cardWidth = width || cardDesktopDefaultWidth;
+  const cardHeight = `calc(${cardWidth} * ${cardRatio})`;
+  const borderRadius = `calc(${cardWidth} / 12)`;
+
+  return css`
+    width: ${cardWidth};
+    height: ${cardHeight};
+    border-radius: ${borderRadius};
+  `;
+}
+
+const CardStyle = styled.div`
+  ${getCardDimensionsStyle}
+  background: no-repeat center/100% url(${getCardSource});
+`;
+
 
 const Card = (props: ICardProps) => {
-  const { card } = props;
-  /** Source da imagem da carta */
-  const cardSource = cardFile[card];
-
   return (
-    <img {...props} src={cardSource}></img>
+    <CardStyle {...props}/>
   )
 }
 
